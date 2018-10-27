b5834083c81479b1a7d728b6333cf977282358ef
守 小室 <hipukon2@gmail.com>
2018-10-27 19:24:37 +0900

    プロジェクト ファイルを追加します。

---
+++ QRreading/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QRreading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
